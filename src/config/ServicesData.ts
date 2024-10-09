3f175b12e1c87008dde9607ba907421c52383cbf
NachoXS-NET <pantojaignacio642@gmail.com>
2024-10-09 20:33:06 -0300

    optimized services, fixed visual bugs

---
+++ src/config/ServicesData.ts
@@ -0,0 +1,44 @@
+const servicesData = [
+    {
+        "bg": "/assets/",
+        "title": "Tecnología",
+        "info": [
+            "Desarrollo de Software a medida",
+            "Página web y tiendas virtuales",
+            "Plataformas virtuales de aprendizaje",
+            "Analisis de datos"
+        ]
+    },
+    {
+        "bg": "/assets/",
+        "title": "Tecnología",
+        "info": [
+            "Desarrollo de Software a medida",
+            "Página web y tiendas virtuales",
+            "Plataformas virtuales de aprendizaje",
+            "Analisis de datos"
+        ]
+    },
+    {
+        "bg": "/assets/",
+        "title": "Tecnología",
+        "info": [
+            "Desarrollo de Software a medida",
+            "Página web y tiendas virtuales",
+            "Plataformas virtuales de aprendizaje",
+            "Analisis de datos"
+        ]
+    },
+    {
+        "bg": "/assets/",
+        "title": "Tecnología",
+        "info": [
+            "Desarrollo de Software a medida",
+            "Página web y tiendas virtuales",
+            "Plataformas virtuales de aprendizaje",
+            "Analisis de datos"
+        ]
+    }
+]
+
+export default servicesData
