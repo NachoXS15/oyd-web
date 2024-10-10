e88b06ba5e3f583221456ee0b29fd00ca02f998c
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-10-10 11:49:23 -0300

    updated info and fixed bugs

--- src/config/ServicesData.ts
+++ src/config/ServicesData.ts
@@ -1,6 +1,6 @@
 const servicesData = [
     {
-        "bg": "/assets/",
+        "bg": "/assets/services/tech.jpeg",
         "title": "Tecnología",
         "info": [
             "Desarrollo de Software a medida",
@@ -10,33 +10,35 @@ const servicesData = [
         ]
     },
     {
-        "bg": "/assets/",
-        "title": "Tecnología",
+        "bg": "/assets/services/admin.webp",
+        "title": "Administración",
         "info": [
-            "Desarrollo de Software a medida",
-            "Página web y tiendas virtuales",
-            "Plataformas virtuales de aprendizaje",
-            "Analisis de datos"
+            "Planificación estratégica",
+            "Estratégias de marketing",
+            "Diseño de procesos de gestión",
+            "Gestión de calidad",
+            "Gestión de redes sociales",
+            
         ]
     },
     {
-        "bg": "/assets/",
-        "title": "Tecnología",
+        "bg": "/assets/services/rrhh.webp",
+        "title": "Recursos Humanos",
         "info": [
-            "Desarrollo de Software a medida",
-            "Página web y tiendas virtuales",
-            "Plataformas virtuales de aprendizaje",
-            "Analisis de datos"
+            "Formación gerencial",
+            "Capaticaciones a medida",
+            "Selección de personal y psicotécnico"
         ]
     },
     {
-        "bg": "/assets/",
-        "title": "Tecnología",
+        "bg": "/assets/services/proyecto.jpeg",
+        "title": "Proyectos",
         "info": [
-            "Desarrollo de Software a medida",
-            "Página web y tiendas virtuales",
-            "Plataformas virtuales de aprendizaje",
-            "Analisis de datos"
+            "Investigación de mercado",
+            "Planes de negocio y proyectos",
+            "Gestión de fondos",
+            "Registro de marcas",
+            "Desarrollo de franquicias"
         ]
     }
 ]
